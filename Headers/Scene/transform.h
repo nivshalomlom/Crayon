1d20c5bf472942299d31d7e2d3506ee8769f1345
Niv Shalom <nivshalomlom@gmail.com>
2023-04-20 20:29:41 +0300

    Renamed Transform

--- Headers/Scene/transform.h
+++ Headers/Scene/transform.h
@@ -6,17 +6,17 @@
 class Transform
 {
     private:
-        glm::mat4 transform;
+        glm::mat4 localToWorld;
 
     public:
         Transform(glm::vec3 position = glm::vec3(0), glm::vec3 lookkAt = glm::vec3(0, 0, 1), glm::vec3 up = glm::vec3(0, 1, 0))
         {
-            this->transform = glm::translate(glm::identity<glm::mat4>(), position);
+            this->localToWorld = glm::translate(glm::identity<glm::mat4>(), position);
         }
 
         Transform(glm::mat4 transform)
         {
-            this->transform = transform;
+            this->localToWorld = transform;
         }
 
         glm::vec3 Position();
@@ -27,13 +27,13 @@ class Transform
 
         glm::vec3 Right();
 
-        void Scale(glm::vec3 scale) { this->transform = glm::scale(this->transform, scale); }
+        void Scale(glm::vec3 scale) { this->localToWorld = glm::scale(this->localToWorld, scale); }
 
-        void LookAt(glm::vec3 position) { this->transform *= glm::lookAt(this->Position(), position, this->Up()); }
+        void LookAt(glm::vec3 position) { this->localToWorld *= glm::lookAt(this->Position(), position, this->Up()); }
 
-        void Rotate(glm::vec3 axis, float angle) { this->transform = glm::rotate(this->transform, glm::radians(angle), axis); };
+        void Rotate(glm::vec3 axis, float angle) { this->localToWorld = glm::rotate(this->localToWorld, glm::radians(angle), axis); };
 
-        void Translate(glm::vec3 translation) { this->transform = glm::translate(this->transform, translation); }
+        void Translate(glm::vec3 translation) { this->localToWorld = glm::translate(this->localToWorld, translation); }
 };
 
 #endif
--- Sources/transform.cpp
+++ Sources/transform.cpp
@@ -12,10 +12,10 @@ vec4 GetMatrixCol(mat4 matrix, int col)
     );
 }
 
-vec3 Transform::Position() { return GetMatrixCol(this->transform, 3).xyz(); }
+vec3 Transform::Position() { return GetMatrixCol(this->localToWorld, 3).xyz(); }
 
-vec3 Transform::Forward() { return GetMatrixCol(this->transform, 2).xyz(); }
+vec3 Transform::Forward() { return GetMatrixCol(this->localToWorld, 2).xyz(); }
 
-vec3 Transform::Up() { return GetMatrixCol(this->transform, 1).xyz(); }
+vec3 Transform::Up() { return GetMatrixCol(this->localToWorld, 1).xyz(); }
 
-vec3 Transform::Right() { return GetMatrixCol(this->transform, 0).xyz(); }
+vec3 Transform::Right() { return GetMatrixCol(this->localToWorld, 0).xyz(); }

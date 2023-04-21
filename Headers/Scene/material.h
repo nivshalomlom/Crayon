c86a229b214235ac615932cc46e19dcef0939832
Niv Shalom <nivshalomlom@gmail.com>
2023-04-21 12:12:04 +0300

    Added Emission Strength

--- Headers/Scene/material.h
+++ Headers/Scene/material.h
@@ -9,22 +9,20 @@ struct Material
         glm::vec4 emissionColor;
         glm::vec4 baseColor;
         
+        float emissionStrength;
         float reflectance;
         float roughness;
         float metallic;
         
-        Material(glm::vec3 baseColor = glm::vec3(0), glm::vec3 emissionColor = glm::vec3(0), float metallic = 0, float roughness = 0, float reflectance = 0)
+        Material(glm::vec3 baseColor = glm::vec3(0), glm::vec3 emissionColor = glm::vec3(0), float emissionStrength = 0, float metallic = 0, float roughness = 0, float reflectance = 0)
         {
             this->emissionColor = glm::vec4(emissionColor, 1.0f);
             this->baseColor = glm::vec4(baseColor, 1.0f);
+            this->emissionStrength = emissionStrength;
             this->reflectance = reflectance;
             this->roughness = roughness;
             this->metallic = metallic;
         }
-
-    private:
-        // This is only here to make the material size divisable by 4 to work with std430 rules
-        float padding = 0;
 };
 
 #endif

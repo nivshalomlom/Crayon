66892cf8bad9b34a6595a55832809a371f20fa45
Niv Shalom <nivshalomlom@gmail.com>
2023-04-22 17:11:22 +0300

    Upgraded Include

--- Sources/program.cpp
+++ Sources/program.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 const static string INCLUDE_DIRECTIVE = "#include \"";
+const static string STAY_IN_ROOT = "./";
+const static string GO_TO_PARENT = "../";
 
 string WorkingDirectory(string filePath)
 {
@@ -17,13 +19,37 @@ string WorkingDirectory(string filePath)
     return "";
 }
 
+string CreateRelativePath(string workingDirectory, string path)
+{
+    string relative = workingDirectory;
+    while (true)
+    {
+        if (path.rfind(STAY_IN_ROOT) == 0)
+            path = path.substr(STAY_IN_ROOT.length());
+        else if (path.rfind(GO_TO_PARENT) == 0)
+        {
+            path = path.substr(GO_TO_PARENT.length());
+            int edge = relative.length() - 2;
+
+            while (relative[edge] != '/')
+                edge--;
+
+            relative = relative.substr(0, edge + 1);
+        }
+        else
+            break;
+    }
+
+    return relative + path;
+}
+
 string ParseShader(string shaderPath)
 {
     ifstream shader(shaderPath);
     string output = "", line;
 
     if (!shader.is_open())
-        return output;
+        throw new runtime_error("Can't find shader at provided path!: " + shaderPath);
 
     string workingDirectory = WorkingDirectory(shaderPath);
     while (getline(shader, line))
@@ -37,7 +63,7 @@ string ParseShader(string shaderPath)
             line = line.substr(INCLUDE_DIRECTIVE.length());
             line.pop_back();
 
-            output += ParseShader(workingDirectory + line);
+            output += ParseShader(CreateRelativePath(workingDirectory, line));
         }
         else
             output += line + '\n';

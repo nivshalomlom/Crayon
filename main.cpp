34632bf431c07618a0943c53ae90b16aa6a92a42
Niv Shalom <nivshalomlom@gmail.com>
2023-05-09 22:01:59 +0300

    Added Rendering Support For Planes

--- main.cpp
+++ main.cpp
@@ -26,12 +26,13 @@ return color
 int main(int argc, char **argv)
 {
     Window window = Window(1280, 720, "Ray Tracing Demo", 60);
-    Camera camera = Camera(vec3(0, 0, 0), 25.0f, 16.0f / 9.0f, 5, 20);
+    Camera camera = Camera(vec3(0, 0, 0), 30.0f, 16.0f / 9.0f, 5, 20);
     Scene scene = Scene();
 
-    scene.Add(Sphere(vec3(3, -1, 20), 2, Material(vec3(0.5, 0.0, 0.0))), SPHERE_TYPE);
-    scene.Add(Sphere(vec3(-3, -1, 20), 2, Material(vec3(0.0, 0.5, 0.0))), SPHERE_TYPE);
-    scene.Add(Sphere(vec3(0, 2, 20), 2, Material(vec3(0.0, 0.0, 0.5))), SPHERE_TYPE);
+    scene.Add(Sphere(vec3(-5, -1, 20), 1, Material(vec3(0.5, 0.0, 0.0))), SPHERE_TYPE);
+    scene.Add(Sphere(vec3(-1, 0, 20), 2, Material(vec3(0.0, 0.5, 0.0))), SPHERE_TYPE);
+    scene.Add(Sphere(vec3(4.5, 1, 20), 3, Material(vec3(0.0, 0.0, 0.5))), SPHERE_TYPE);
+    scene.Add(Plane(vec3(0, -2, 20), vec3(7.0), Material(vec3(0.0, 0.5, 0.5))), PLANE_TYPE);
     SceneRenderer* sceneRenderer = new SceneRenderer(scene, camera, 1280, 720);
 
     TextureRenderer* textureRenderer = new TextureRenderer();

9fc194fa4fa91a1319d18edfbd3b5f246a4c7c6d
Niv Shalom <nivshalomlom@gmail.com>
2023-06-16 19:42:59 +0300

    Tone Mapping Program

---
+++ Headers/PostProccessing/tone_mapping.h
@@ -0,0 +1,29 @@
+#ifndef _TONE_MAPPING_H
+#define _TONE_MAPPING_H
+
+#include "../Graphics/program.h"
+
+class ToneMappingProgram : public PostProcesingProgram
+{
+    private:
+        GLuint textureLocation;
+
+    public:
+        ToneMappingProgram(float exposure = 1.0, float gamma = 2.2) : PostProcesingProgram("./Shaders/Fragment/tone.frag")
+        {
+            this->textureLocation = glGetUniformLocation(this->Id(), "textureSampler");
+            glUniform1f(glGetUniformLocation(this->Id(), "exposure"), exposure);
+            glUniform1f(glGetUniformLocation(this->Id(), "gamma"), gamma);
+        }
+
+        void LoadSourceTexture(const Texture2D source)
+        {
+            this->Mount();
+            source.Bind();
+
+            glActiveTexture(GL_TEXTURE0);
+            glUniform1i(this->textureLocation, 0);
+        }
+};
+
+#endif

d7f9570133facb0e808f9315c428d883e46e0923
Niv Shalom <nivshalomlom@gmail.com>
2023-06-23 02:44:11 +0300

    1080p

--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@
 #include "./Headers/PostProcessing/bloom.h"
 #include "./Headers/PostProcessing/tone_mapping.h"
 
+#define WIDTH 1920
+#define HEIGHT 1080
+
 using namespace std;
 using namespace glm;
 
 int main(int argc, char **argv)
 {
-    Window window = Window(1280, 720, "Ray Tracing Demo", 60);
-    Camera camera = Camera(vec3(0, 0, 0), 30.0f, 16.0f / 9.0f, 5, 20);
+    Window window = Window(WIDTH, HEIGHT, "Ray Tracing Demo", 60);
+    Camera camera = Camera(vec3(0, 0, 0), 30.0f, float(WIDTH) / HEIGHT, 5, 20);
 
     Scene scene = Scene();
     scene.Add(Sphere(vec3(-5, -1, 20), 1, Material(vec3(0.0), vec3(100, 50, 0), 1.0)), SPHERE_TYPE);
@@ -18,7 +21,7 @@ int main(int argc, char **argv)
     scene.Add(Sphere(vec3(4.5, 1, 20), 3, Material(vec3(0.0), vec3(0, 50, 100), 1.0)), SPHERE_TYPE);
     scene.Add(Plane(vec3(0, -2, 20), vec3(7.0), Material(vec3(1.0))), PLANE_TYPE);
 
-    SceneRenderer* sceneRenderer = new SceneRenderer(scene, camera, 1280, 720);
+    SceneRenderer* sceneRenderer = new SceneRenderer(scene, camera, WIDTH, HEIGHT);
     TextureBuffer* textureRenderer = new TextureBlitBuffer();
 
     Texture2D renderTexture = sceneRenderer->RenderTexture();
@@ -29,7 +32,7 @@ int main(int argc, char **argv)
     window.Show([sceneRenderer, textureRenderer, renderTexture](Window* window, float dt) 
     {
         sceneRenderer->Render();
-        textureRenderer->RenderToBuffer(renderTexture, ivec2(0), ivec2(1280, 720));
+        textureRenderer->RenderToBuffer(renderTexture, ivec2(0), ivec2(WIDTH, HEIGHT));
     });
 
     window.Dispose();
